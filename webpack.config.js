de6779642bfe641bd028929a3ec70a7244f67b04
Kamil Chmielewski <kamil.chm@gmail.com>
2017-10-21 11:37:07 +0200

    generate app manifest

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 var path = require('path')
 
+var WebpackPwaManifest = require('webpack-pwa-manifest')
+
 const elmMake = process.env.ELM_MAKE || 'elm-make';
 
 module.exports = {
@@ -15,41 +17,52 @@ module.exports = {
     },
 
     module: {
-        rules: [
-            {
-                test: /\.(css|scss)$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                ]
-            },
+      rules: [
+        {
+          test: /\.(css|scss)$/,
+          use: [
+            'style-loader',
+            'css-loader',
+          ]
+        },
+        {
+          test:    /\.html$/,
+          exclude: /node_modules/,
+          loader:  'file-loader?name=[name].[ext]',
+        },
+        {
+          test:    /\.elm$/,
+          exclude: [/elm-stuff/, /node_modules/],
+          loader:  `elm-webpack-loader?debug=true&verbose=true&warn=true&pathToMake=${elmMake}`,
+        },
+        {
+          test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+        },
+        {
+          test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: 'file-loader',
+        },
+      ],
 
-          {
-            test:    /\.html$/,
-            exclude: /node_modules/,
-            loader:  'file-loader?name=[name].[ext]',
-          },
-          {
-            test:    /\.elm$/,
-            exclude: [/elm-stuff/, /node_modules/],
-            loader:  `elm-webpack-loader?debug=true&verbose=true&warn=true&pathToMake=${elmMake}`,
-          },
-          {
-            test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-            loader: 'url-loader?limit=10000&mimetype=application/font-woff',
-          },
-          {
-            test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-            loader: 'file-loader',
-          },
-        ],
+      noParse: /\.elm$/,
+    },
 
-        noParse: /\.elm$/,
-      },
+    plugins: [
+      new WebpackPwaManifest({
+        name: 'tudu',
+        short_name: 'tudu',
+        description: 'so many things to do',
+        background_color: '#209cee',
+        theme_color: '#209cee',
+        display: 'standalone',
+        fingerprints: false,
+        icons: [],
+      })
+    ],
 
-      devServer: {
-        inline: true,
-        stats: { colors: true },
-      },
-
+    devServer: {
+      inline: true,
+      stats: { colors: true },
+    },
 };
