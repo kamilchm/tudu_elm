bd972d2cbe76056263f353edde3f48fa6db68d8d
Kamil Chmielewski <kamil.chm@gmail.com>
2018-02-06 10:04:35 +0100

    use nix mkShell

--- shell.nix
+++ shell.nix
@@ -1,11 +1,10 @@
 with import <nixpkgs> {};
 
-stdenv.mkDerivation rec {
-  name = "tudu-dev-shell";
-  
+mkShell rec {
   buildInputs = [ yarn elmPackages.elm ];
 
   shellHook = ''
     export ELM_MAKE=${elmPackages.elm}/bin/elm-make
+    export PATH="`pwd`/node_modules/.bin:$PATH"
   '';
 }
